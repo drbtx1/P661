6af8b03c69ff235b8de65060c93eaecaf4ded9d3
drbtx1 <drbtx1@users.noreply.github.com>
2025-02-04 22:19:03 -0500

    Add files via upload

--- reduced density matrix.py
+++ reduced density matrix.py
@@ -7,26 +7,28 @@ Created on Wed Jan 29 10:53:11 2025
 import numpy as np
 #import random
 lenA = 2
-lenB = 4
+lenB = 2
 
-psiA = np.zeros((lenA,1))
-psiB = np.zeros((lenB,1))
+psiA = np.empty([lenA,1]).astype(complex)
+psiB = np.empty([lenB,1]).astype(complex)
 
 
 #populate with random integers
 sqtermsA = 0
 for element in range(len(psiA)):
     value = np.random.randint(-10,10)
-    psiA[element] = value
-    sqtermsA += value**2
+    imag = np.random.randint(-10,10)
+    psiA[element] = value + imag*1j
+    sqtermsA += (value**2 + imag*np.conj(imag))
 #normalize   
 psiA = psiA/np.sqrt(sqtermsA)    
     
 sqtermsB = 0
 for element in range(len(psiB)):
     value = np.random.randint(-10,10)
-    psiB[element] = value
-    sqtermsB += value**2
+    imag = np.random.randint(-10,10)
+    psiB[element] = value + imag*1j
+    sqtermsB += (value**2 + imag*np.conj(imag))
 psiB = psiB/np.sqrt(sqtermsB)
 
 rhoA = psiA@(np.conj(psiA.T))  
@@ -38,9 +40,11 @@ print(psiB)
 print(rhoB)
 M = np.kron(rhoA,rhoB)
 print(M)
+psi = np.kron(psiA,psiB)
+#print(psi@np.conj(psi.T))
 
 def partialtraceA(M,a,b):
-    reduced = np.zeros((a,a))
+    reduced = np.empty((a,a)).astype(complex)
     for i in range(a):
         for j in range(a):
             sum = 0
@@ -52,7 +56,7 @@ def partialtraceA(M,a,b):
 print("reduced density matrix A:\n" + str(partialtraceA(M,lenA,lenB)))  
 
 def partialtraceB(M,a,b):
-    reduced = np.zeros((b,b))
+    reduced = np.empty((b,b)).astype(complex)
     for i in range(b):
         for j in range(b):
             sum = 0
